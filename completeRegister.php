636ad6d9c0deec97bb06ae588249dbbd01df6603
vitor <vitor.eg90@gmail.com>
2024-05-09 19:14:05 -0300

    atualização places completeRegister

--- completeRegister.php
+++ completeRegister.php
@@ -1,10 +1,9 @@
 <?php
 require("usrclass.php");
 session_start();
-if (isset($_SESSION['usr_obj'])){
+if (isset($_SESSION['usr_obj'])) {
     $usu_obj = unserialize($_SESSION['usr_obj']);
-}
-else {
+} else {
     header("location: index.php");
 }
 //Detalhe: Isso aqui é só pra colocar o primeiro nome alí no cabeçalho, é frescura, mas é legal
@@ -12,23 +11,23 @@ $nome = $usu_obj->primeiroNome();
 
 $icone = 'Escolha seu ícone';
 
-if(isset($_POST['icon'])){
+if (isset($_POST['icon'])) {
     $icone = $_POST['icon'];
-    if($icone == 'Icone tipo A'){
+    if ($icone == 'Icone tipo A') {
         $caminho = 'assets\\\images\\\iconsRegister\\\iconOpt1.png';
-    }else{
-        if($icone == 'Icone tipo B'){
+    } else {
+        if ($icone == 'Icone tipo B') {
             $caminho = 'assets\\\images\\\iconsRegister\\\iconOpt2.png';
-        }else{
-            if($icone == 'Icone tipo C'){
+        } else {
+            if ($icone == 'Icone tipo C') {
                 $caminho = 'assets\\\images\\\iconsRegister\\\iconOpt3.png';
             }
         }
     }
-}else{
+} else {
     $caminho = 'assets\\\images\\\iconsRegister\\\iconOptNull.png';
 }
-    
+
 
 ?>
 
@@ -41,7 +40,7 @@ if(isset($_POST['icon'])){
     <title>Complete Registration</title>
     <link rel="stylesheet" href="./assets/css/registerStyle.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+
 </head>
 
 <body>
@@ -49,11 +48,11 @@ if(isset($_POST['icon'])){
         <div class="form complete-registration">
             <header> <?php echo $nome; ?>, Conte mais sobre você</header>
             <form id="completaRegistr0" action="BackCompletaRegistro.php" method="POST">
-            <label> Qual seu número de celular</label>
+                <label> Qual seu número de celular</label>
                 <input type="text" id="celular" name="celular" placeholder="(xx) xxxxx-xxxx" required pattern="\([0-9]{2}\) [0-9]{5}-[0-9]{4}">
                 <label> Como pretende usar este sistema? </label>
                 <select id="tipoUser" name="tipoUser">
-                    <option value="0">Para solcitar coletas</option>
+                    <option value="0">Para solicitar coletas</option>
                     <option value="1">Para coletar resíduos</option>
                 </select>
                 <label> Que tal um Ícone? </label>
@@ -66,7 +65,7 @@ if(isset($_POST['icon'])){
                     <option value="assets\\images\\iconsRegister\\iconOpt2.png">Ícone B</option>
                     <option value="assets\\images\\iconsRegister\\iconOpt3.png">Ícone C</option>
                 </select> -->
-                
+
                 <!-- Aqui nós fazemos a checagem do endereço pela API -->
                 <label> Qual o endereço ou CEP de sua residência? </label>
                 <input type="text" id="end" name="end" placeholder="Endereço ou CEP">
@@ -76,7 +75,7 @@ if(isset($_POST['icon'])){
             </form>
         </div>
     </section>
-    
+
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
@@ -106,11 +105,27 @@ if(isset($_POST['icon'])){
     <!-- script bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
+    <!-- script Google Places Autocomplete -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjiJnJKpcL9tMRGfD9AGmPYZPmydig87g&libraries=places"></script>
+
+    <script>
+        // Inicializar o autocompletar no campo de endereço
+        var autocomplete = new google.maps.places.Autocomplete(document.getElementById('end'));
+
+        // Definir o tipo de autocompletar para endereços
+        autocomplete.setTypes(['address']);
+    </script>
+
+    <!-- script bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <script>
         // Function to format phone number input
         function formatPhoneNumber(event) {
             const input = event.target;
-            const { value } = input;
+            const {
+                value
+            } = input;
 
             // Remove all non-numeric characters
             const phoneNumber = value.replace(/\D/g, '');
@@ -119,8 +134,8 @@ if(isset($_POST['icon'])){
             if (phoneNumber.length > 0) {
                 // Apply phone number formatting
                 let formattedPhoneNumber = '(' + phoneNumber.substring(0, 2) + ') ' +
-                                            phoneNumber.substring(2, 7) + '-' +
-                                            phoneNumber.substring(7, 11);
+                    phoneNumber.substring(2, 7) + '-' +
+                    phoneNumber.substring(7, 11);
 
                 // Update input value with formatted phone number
                 input.value = formattedPhoneNumber;
--- conexao.php
+++ conexao.php
@@ -10,7 +10,7 @@ class conexaoBD
     function __construct(){
         $this->dns = 'mysql:host=127.0.0.1;port=3306;dbname=prolixo';
         $this->username = 'root';
-        $this->password = '1234';
+        $this->password = '';
         $this->connection = new PDO($this->dns, $this->username, $this->password);
         //PS: N estou certo quanto à necessidade do atributo connection, mas vou deixar aqui por precaução.
         //Ele funciona, só que é mais rolê usar ele do que criar um PDO novo a cada página.
