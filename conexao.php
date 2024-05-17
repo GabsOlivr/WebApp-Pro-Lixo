a3a8ba92f819569d6121e18e2a207b3b1e8eebc8
vitor <vitor.eg90@gmail.com>
2024-05-17 15:26:39 -0300

    teste

--- conexao.php
+++ conexao.php
@@ -11,6 +11,7 @@ class conexaoBD
         $this->dns = 'mysql:host=127.0.0.1;port=3306;dbname=prolixo';
         $this->username = 'root';
         $this->password = '';
+        
         $this->connection = new PDO($this->dns, $this->username, $this->password);
         //PS: N estou certo quanto à necessidade do atributo connection, mas vou deixar aqui por precaução.
         //Ele funciona, só que é mais rolê usar ele do que criar um PDO novo a cada página.
